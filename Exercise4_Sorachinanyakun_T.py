1ed3148bb20690de8b4c116e1e7c813a8b3b165f
Sorachinan <u6019680161@lsed.tu.ac.th>
2022-03-18 19:46:29 +0700

    add text
    ใส่ข้อมูลลงไป1

---
+++ Exercise4_Sorachinanyakun_T.py
@@ -0,0 +1,11 @@
+
+print("มหาวิทยาลัยชื่อดัง", "คณะวิศวกรรมศาสตร์", "ชั้นปีที่2")
+print("--Your Score--")
+FE="คะแนนที่ได้"
+GB="คะแนนที่ได้"
+ICS="คะแนนที่ได้"
+CP="คะแนนที่ได้"
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
